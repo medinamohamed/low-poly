7a05ea947904a5de447be0adfb88eff77913cf01
medinamohamed <medina.ahmed.mo@gmail.com>
2020-05-09 13:47:21 -0400

    completed ran triangles

--- sketch.js
+++ sketch.js
@@ -1,104 +1,121 @@
+let n = 2 // length of an triangle in pixel
 
-let r;  // ???
-let n = 3  // length of an triangle in pixel
-
-const pixel = 30; // 1 pixel is equal to a distance of 30
+const pixel = 25; // 1 pixel is equal to a distance of 30
 
 function setup() {
-  
-  canva = createCanvas(780, 780);
+
+    let l = 27
+    canva = createCanvas(pixel*l, pixel*l);
+
 
     // Plan cartesien
-  for(let j=1 ; j < 26 ; j++){
-      for (let i=1 ; i < 26; i++){
-        rect(pixel*i, pixel*j, pixel, pixel);
+    for (let j = 1; j < l; j++) {
+        for (let i = 1; i < l; i++) {
+            rect(pixel * i, pixel * j, pixel, pixel);
+        }
     }
-  }
 
-  // map the coordinate of point and convert distance to pixel
-function point(x,y){
+    // map the coordinate of point and convert distance to pixel
+    function point(x, y) {
 
-  if (x == 0 && y ==0){
-    return {x :pixel, y:pixel};
-  }
-  if (x != 0 && y ==0){
-    return {x :pixel*(x+1), y:pixel};
-  }
-  if (x == 0 && y !=0){
-    return {x :pixel, y:pixel*(y+1)};
-  }
-  if (x != 0 && y !=0){
-    return {x :pixel*(x+1), y:pixel*(y+1)};
-  }
-
-}
-
-
-function initializeCoordinate(column){
-
-  let initializeCoordinate = [point(0,0)]
-
-  for (let i = 0; i< column;i++){
-    initializeCoordinate.push(point(n+(n*i),0))
-  }
-  return initializeCoordinate;
-
-}
-
-function drawTriangle(A,B,C){
-  triangle(A.x, A.y ,B.x,B.y,C.x,C.y);
-}
-
-function newTriangle(A,B,C){
-  currentPoints = [A,B,C];   
-  drawTriangle.apply(this,currentPoints); 
-}
-
-let previousCoordinate = []
-let current;
-function drawOneRow(row,initializeCoordinate){
-
-    let A = initializeCoordinate[0]
-    let B = point(0,row*n) 
-    previousCoordinate.push(B)
-
-    let len = initializeCoordinate.length/2;
-    for (let i = 1; i < len;i++){
-
-      current = (2*i)-1
-      let C = initializeCoordinate[current]
-
-      newTriangle(A,B,C)     // Triangle 1 
-      
-      A = point(current*n,row*n)
-      previousCoordinate.push(A)
-      newTriangle(A,B,C)    // Triangle  2
-  
-      current = 2*i
-
-      B = point(current*n,row*n)
-      previousCoordinate.push(B)
-      newTriangle(A,B,C)    // Triangle 3
-  
-      
-      A = initializeCoordinate[current]
-      newTriangle(A,B,C);   // Triangle 4
+        if (x == 0 && y == 0) {
+            return {
+                x: pixel,
+                y: pixel
+            };
+        }
+        if (x != 0 && y == 0) {
+            return {
+                x: pixel * (x + 1),
+                y: pixel
+            };
+        }
+        if (x == 0 && y != 0) {
+            return {
+                x: pixel,
+                y: pixel * (y + 1)
+            };
+        }
+        if (x != 0 && y != 0) {
+            return {
+                x: pixel * (x + 1),
+                y: pixel * (y + 1)
+            };
+        }
 
     }
-    return previousCoordinate;
-   
-}
 
-// think about recursion ... 
+    function drawTriangle(A, B, C) {
+      triangle(A.x, A.y, B.x, B.y, C.x, C.y);
+    }
 
-let test = drawOneRow(1,initializeCoordinate(8));
+    function newTriangle(A, B, C) {
+        currentPoints = [A, B, C];
+        drawTriangle.apply(this, currentPoints);
+    }
 
-let test2 = drawOneRow(2,test);
+    function initializeCoordinate(column) {
 
-//let test3 = drawOneRow(3,test2);
+        let initializeCoordinate = [point(0, 0)]
+
+        for (let i = 0; i < column; i++) {
+            initializeCoordinate.push(point(n + (n * i), 0))
+        }
+        return initializeCoordinate;
+
+    }
+
+    let current;
+
+    function drawOneRow(row, initializeCoordinate) {
+        let previousCoordinate = []
+    
+        let A = initializeCoordinate[0]
+        let B = point(0, (row * n))
+        previousCoordinate.push(B)
+        let len = initializeCoordinate.length / 2;
+        for (let i = 1; i < len; i++) {
+            let ran = Math.random()*2;
+            current = (2 * i) - 1
+            let C = initializeCoordinate[current]
+
+            newTriangle(A, B, C) // Triangle 1 
+
+            A = point(current * n, (row * n)+ran)
+        
+            previousCoordinate.push(A)
+            newTriangle(A, B, C) // Triangle  2
+
+            current = 2 * i
+
+            B = point(current * n, row * n)
+            previousCoordinate.push(B)
+            newTriangle(A, B, C) // Triangle 3
 
 
-function draw() {
-  // put drawing code here
-}
+            A = initializeCoordinate[current]
+            newTriangle(A, B, C); // Triangle 4
+
+        }
+        console.log(previousCoordinate)
+        //todo before returning change randomnly the n to 3 , 4 or 5 except for last row
+        return previousCoordinate;
+
+    }
+
+
+    function drawAllRows(numberOfRows, numberOfColumns) {
+        numberOfRows++;
+        let newCoordinate = drawOneRow(1, initializeCoordinate(numberOfColumns));
+        for (let i = 2; i < numberOfRows; i++) {
+          newCoordinate = drawOneRow(i, newCoordinate);
+        }
+
+    }
+    drawAllRows(12, 12);
+
+
+    function draw() {
+        // put drawing code here
+    }
 }
